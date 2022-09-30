bcb6861bb3e767ea7c66728563d181863cb20857
jorg3rr <jorg3x21@gmail.com>
2022-09-30 08:19:57 -0500

    Parcial01 8:19

--- Parcial01/src/mx/TestSeries.java
+++ Parcial01/src/mx/TestSeries.java
@@ -1,13 +1,16 @@
+package mx.parc1.soft.pruebas;
+import mx.parc1.soft.series.*;
 public class TestSeries {
 	public static void main(String[] args) {
 		Serie s = new Serie();
-		int m = s.genNumero(100);
-		s.primos(m);
-		int o = s.primoN(m/10);
-		System.out.println(o);
+		int  
+		//int m = s.genNumero(100);
+		//s.primos(m);
+		//int o = s.primoN(m/10);
+		//System.out.println(o);
 		int n = s.genNumero(100);
 		s.fibo(n);
-		int p = s.fibo(3,11);
+		int p = s.fibo();
 		System.out.println(p);
 	}
 }
---
+++ Parcial01/src/mx/parc1/soft/series/Serie.java
@@ -0,0 +1,10 @@
+package mx.parc1.soft.series;
+public class Serie{
+	public int numero;
+	public Serie(int numero){
+		this.numero = numero;
+	}
+	public String fibo(){
+		
+	}
+}
