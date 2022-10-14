5073fc408b51b4c6bf372cd02b1f8592226e8162
jorg3rr <jorg3x21@gmail.com>
2022-10-14 11:31:18 -0500

    practica09 11:31

--- practica09/src/mx/desasof/inmobiliaria/pruebas/PruebaCasa.java
+++ practica09/src/mx/desasof/inmobiliaria/pruebas/PruebaCasa.java
@@ -5,14 +5,36 @@ import mx.desasof.inmobiliaria.casas.Urbana;
 
 public class PruebaCasa{
 	public static void main(String[] args) {
-		Casa casas[] = {new Casa("Estandar",3,3000000),
-		new Campo("Cazador",3.5f,10f),
-		new Urbana("Residencia",3,2500000,2),
-		new Casa("Estandar",3,3000000),
-		new Campo("Cazador",3.5f,10),
-		new Urbana("Residencia",3,2500000,2),
-		new Casa("Estandar"),
-		new Campo("Cazador",4,15000,3f,5f),
-		new Urbana("Residencia",5)};
+		Casa casas[] = {new Casa(" Estandar",3,3000000),
+		new Campo(" Cazador",3.5f,10f),
+		new Urbana(" Residencia",3,2500000,2),
+		new Casa(" Estandar",3,3000000),
+		new Campo(" Cazador",3.5f,10),
+		new Urbana(" Residencia",3,2500000,2),
+		new Casa(" Estandar"),
+		new Campo(" Cazador",4,15000,3f,5f),
+		new Urbana(" Residencia",5)};
+
+		int i = 0;
+		while (i<casas.length){
+			for (Casa c:casas) {
+				if (c == (casas[i])) {
+				 	System.out.println(c + " Son el mismo objeto" + (casas[i]));
+				 	System.out.println(" = = = = = ");
+				 	break;
+				 }
+				 	if (c.equals(casas[i])) {
+				 	 	System.out.println(c + " Son Iguales" + (casas[i]));
+				 	 	System.out.println(" e e e e e ");
+				 	 	break;
+				 	}
+				 		if (c != (casas[i])) {
+				 			System.out.println(c + " Son diferentes " + (casas[i]));
+				 			System.out.println(" ! ! ! ! ! ");
+				 			continue;
+				 		}
+			}//for
+			i++;
+		}//while
 	}
 }
