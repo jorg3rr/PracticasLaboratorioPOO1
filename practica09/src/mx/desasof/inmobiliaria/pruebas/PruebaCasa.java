2942fe767ce7d2c62fb87a074cafb8abb01a4157
jorg3rr <jorg3x21@gmail.com>
2022-10-14 09:50:23 -0500

    practica09 9:50

--- practica09/src/mx/desasof/inmobiliaria/pruebas/PruebaCasa.java
+++ practica09/src/mx/desasof/inmobiliaria/pruebas/PruebaCasa.java
@@ -1,8 +1,18 @@
-package mx.desasof.inmobiliaria.casas;
+package mx.desasof.inmobiliaria.pruebas;
 import mx.desasof.inmobiliaria.casas.Casa;
 import mx.desasof.inmobiliaria.casas.Campo;
-import mx.desasof.inmobiliaria.casas.Hurbanas;
+import mx.desasof.inmobiliaria.casas.Urbana;
 
 public class PruebaCasa{
-	
+	public static void main(String[] args) {
+		Casa casas[] = {new Casa("Estandar",3,3000000),
+		new Campo("Cazador",3.5f,10f),
+		new Urbana("Residencia",3,2500000,2),
+		new Casa("Estandar",3,3000000),
+		new Campo("Cazador",3.5f,10),
+		new Urbana("Residencia",3,2500000,2),
+		new Casa("Estandar"),
+		new Campo("Cazador",4,15000,3f,5f),
+		new Urbana("Residencia",5)};
+	}
 }
