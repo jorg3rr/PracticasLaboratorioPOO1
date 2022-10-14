38d35c0514ef976ff4e114a4408298010a37b62d
jorg3rr <jorg3x21@gmail.com>
2022-10-14 09:18:48 -0500

    practica09 9:18

--- practica09/src/mx/desasof/inmobiliaria/casas/Campo.java
+++ practica09/src/mx/desasof/inmobiliaria/casas/Campo.java
@@ -1,7 +1,26 @@
 package mx.desasof.inmobiliaria.casas;
 public class Campo extends Casa{
-	 float piscina[] = new float[2];
-	 public Campo(String tipo, int hab, float costo){
-
+	float piscina[] = new float[2];
+	public Campo(String tipo, int hab, float costo,float ancho, float largo){
+		super(tipo,hab,costo);
+	 	piscina[0] = ancho;
+	 	piscina[1] = largo;
 	 }
+	public Campo(String tipo,float ancho, float largo){
+		super(tipo);
+		piscina[0] = ancho;
+	 	piscina[1] = largo;
+	}
+	public int hashCode(){
+		return super.hashCode() *(int)(piscina[0] * piscina[1])/5;
+	}
+	public boolean equals(Object ob){
+		if (super.equals(ob) && (ob instanceof Campo)){
+			return true;				
+		}
+		return false;
+	}
+	public String toString(){
+		return super.toString() + " " + piscina[0] + " " + piscina[1];
+	}
 }
