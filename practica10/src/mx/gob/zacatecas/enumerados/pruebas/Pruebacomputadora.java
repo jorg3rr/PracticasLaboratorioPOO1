fafee28b9342fde892e8b525d349261a8c913b87
jorg3rr <jorg3x21@gmail.com>
2022-10-21 09:39:23 -0500

    practica10 9:39

--- practica10/src/mx/gob/zacatecas/enumerados/pruebas/Pruebacomputadora.java
+++ practica10/src/mx/gob/zacatecas/enumerados/pruebas/Pruebacomputadora.java
@@ -1,12 +1,16 @@
+package mx.gob.zacatecas.enumerados.pruebas;
+import mx.gob.zacatecas.enumerados.equipos.*;
+import mx.gob.zacatecas.enumerados.utilerias.*;
 public class PruebaComputadora{
 	//Crear 4 equipos con los datos que queramos, con diferentes valores de DDR1,2,3,4
 	public static void main(String[] args) {
-		Computadora c1 = new Computadora("Dell",DESKTOP,DDR);	
-		Smallfactor c2 = new Smallfactor("Hp",LAPTOP,DDR3,false);	
-		Computadora c3 = new Computadora("Lenovo",DESKTOP,DDR2);	
-		Smallfactor c4 = new Computadora("Toshiba",DESKTOP,DDR4,true);		
-	}
+		Computadora c1 = new Computadora("Dell",Tipo.DESKTOP,Memoria.DDR);	
+		Smallfactor c2 = new Smallfactor("Hp",Tipo.LAPTOP,Memoria.DDR3,false);	
+		Computadora c3 = new Computadora("Lenovo",Tipo.SERVER,Memoria.DDR2);	
+		Smallfactor c4 = new Smallfactor("Toshiba",Tipo.WORKSTATITON,Memoria.DDR4,true);		
 	
-	//arreglo
+		Computadora equip[] = {c1,c2,c3,c4};
+
+	}
 	//fore
 }
