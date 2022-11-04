600cda7547a8e7115a84c9f39b88b49969af53fa
jorg3rr <jorg3x21@gmail.com>
2022-11-04 10:48:05 -0600

    practica12 10:47

--- practica12/src/edu/escuela/gamepz/pruebas/PruebaTablero.java
+++ practica12/src/edu/escuela/gamepz/pruebas/PruebaTablero.java
@@ -7,13 +7,22 @@ public class PruebaTablero{
 	public static void main(String[] args) {
 		Personaje[] datos = {
 			new Planta("David",100),
-			new Zombie("Bianca"),
-			new Planta("Fabian",10,MEDIO),
+			new Zombi("Bianca"),
+			new Planta("Fabian",10,Escudo.MEDIO),
 			new Planta("Almendra",50),
-			new Planta("Ricardo",BAJO),
+			new Planta("Ricardo",Escudo.BAJO),
 			new Planta("Silvia"),
-			new Zombie("Armando",80,false),
-			new Zombie("Josseline",true),
-			new Zombie("Eduardo")};
+			new Zombi("Armando",80,false),
+			new Zombi("Josseline",true),
+			new Zombi("Eduardo")};
+		for (Personaje p: datos) {
+				int pos = (int)(Math.random()*20 - 5);
+				try{
+					Tablero.insertar(p,pos);
+				}catch(PersException pe){
+					System.err.println(pe.getMessage() + " " + pe.getPoss());
+				}
+		}//for
+		Tablero.mostrar();
 	}
 }
--- practica12/src/edu/escuela/gamepz/utils/Tablero.java
+++ practica12/src/edu/escuela/gamepz/utils/Tablero.java
@@ -1,12 +1,12 @@
 package edu.escuela.gamepz.utils;
 import edu.escuela.gamepz.personajes.Personaje; 
 public class Tablero{
-	public int MAX_SIZE = 10;
-	Personaje[] personajes = new Personaje[MAX_SIZE]; 
+	public static int MAX_SIZE = 10;
+	static Personaje[] personajes = new Personaje[MAX_SIZE]; 
 	private Tablero(int MAX_SIZE){
 		this.MAX_SIZE = MAX_SIZE;
 	}
-	public void mostrar(){
+	public static void mostrar(){
 		System.out.println("\nInicio * * * Contenido del arreglo en Tablero");
 		for (Personaje tmp: personajes) {
 			int cont = 0;
@@ -20,14 +20,14 @@ public class Tablero{
 		}
 	}
 	
-	public void insertar(Personaje p, int pos)throws PersException{
+	public static void insertar(Personaje p, int pos)throws PersException{
 		if (pos < 0 || pos > MAX_SIZE) {
 			throw new PersException("Indice fuera de rango",pos);  
 		}
 		personajes[pos] = p;
 	}
 
-	public void borrar(int pos)throws PersException{
+	public static void borrar(int pos)throws PersException{
 		if (pos < 0 || pos > MAX_SIZE) {
 			throw new PersException("Indice fuera de rango",pos);  	
 		}
