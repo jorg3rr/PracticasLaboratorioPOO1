f951f1ee7f0b73d4275a076bc270327359326b40
jorg3rr <jorg3x21@gmail.com>
2022-11-04 09:16:50 -0600

    practica12 9:16

--- practica12/src/edu/escuela/gamepz/utils/Tablero.java
+++ practica12/src/edu/escuela/gamepz/utils/Tablero.java
@@ -1,9 +1,20 @@
 package edu.escuela.gamepz.utils;
+import edu.escuela.gamepz.personajes.Personaje; 
 public class Tablero{
 	public int MAX_SIZE = 10;
-	public Personaje[] personajes = {MAX_SIZE};
-	private Tablero(int MAX_SIZE, Personaje personajes){
+	Personaje[] personajes = new Personaje[MAX_SIZE]; 
+	private Tablero(int MAX_SIZE){
 		this.MAX_SIZE = MAX_SIZE;
-		this.personajes = personajes;
+	}
+	public void mostrar(){
+		System.out.println("\nInicio * * * Contenido del arreglo en Tablero");
+		for (Personaje tmp: personajes) {
+			if (tmp == null) {
+				System.out.println(tmp + "---");
+			}else{
+				System.out.println();
+			}
+			System.out.println("Fin * * * Contenido del arreglo en Tablero\n");
+		}
 	}
 }
