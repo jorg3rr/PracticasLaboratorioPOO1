8b31a7d7e8372b19b64e2d8f40c0732703244f46
jorg3rr <jorg3x21@gmail.com>
2022-11-11 09:45:31 -0600

    practica13 9:45

---
+++ practica13/src/edu/escuela/gamepz/personajes/buenos/Planta.java
@@ -0,0 +1,51 @@
+package edu.escuela.gamepz.personajes.buenos;
+import edu.escuela.gamepz.personajes.Personaje;
+import edu.escuela.gamepz.utils.Escudo;
+public class Planta extends Personaje{
+    public Escudo escudo;
+    
+    public Escudo getEscudo(){
+        return escudo;
+    }
+
+    ///Constructores importando los atributos de la clase super
+    public Planta(String nombre, int vida, Escudo escudo){
+        super(nombre, vida);
+        this.escudo = escudo;
+    }
+    public Planta(String nombre, Escudo escudo){
+        super(nombre, 3);
+        this.escudo = escudo;
+    }
+    public Planta(String nombre, int vida){
+        super(nombre, vida);
+        escudo = Escudo.NULO;
+    }
+    public Planta(String nombre){
+        super(nombre, 3);
+        escudo = Escudo.NULO;
+    }
+
+    ///Detalles de metodo getDetalle
+    public String toString(){
+        return super.toString() + " " + escudo.getNivel();
+    }
+    ///Decrementos
+    public void decVida(){
+        setVida(vida - escudo.getNivel());
+    }
+    public void decVida(int nivel){
+        setVida(vida - escudo.getNivel()*nivel);
+    }
+    ///Incrementos
+    public void addVida(){
+        setVida(vida + escudo.getNivel());
+    }
+
+    public void addVida(int nivel){
+        setVida(vida + escudo.getNivel()*nivel);
+
+
+    }
+
+}
