7a43af7f342984b1e049f0df0079eef27749cf22
jorg3rr <jorg3x21@gmail.com>
2022-11-25 08:41:03 -0600

    practica15 8:40

--- practica15/src/mx/com/hilos/corredores/ventanas/Ventana.java
+++ practica15/src/mx/com/hilos/corredores/ventanas/Ventana.java
@@ -2,6 +2,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.BorderFactory;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 public class Ventana extends JFrame{
 	private JLabel lblTitulo, lblElem1, lblElem2, hElem1, hElem2;
 	private JTextField txtElem1, txtElem2;
@@ -24,5 +29,11 @@ public class Ventana extends JFrame{
 		txtElem2 = new JTextField(3);
 		btnStart = new JButton("Comenzar");
 		hElem1 = new JLabel(" Aqui va el hilo 1 ");
+		hElem1.setBorder(BorderFactory.createLineBorder(border));
+		hElem1.setPreferredSize(lblDim);
+		hElem2 = new JLabel(" Aqui va el hilo 2 ");
+		hElem2.setBorder(BorderFactory.createLineBorder(border));
+		hElem2.setPreferredSize(lblDim);
+		btnStart.setPreferredSize(lblDim);
 	}
 }
