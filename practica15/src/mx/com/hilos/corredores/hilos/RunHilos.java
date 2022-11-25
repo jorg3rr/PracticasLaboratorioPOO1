ec0e12cc660ecf14783d370ea6cd9f60c1f6b16d
jorg3rr <jorg3x21@gmail.com>
2022-11-25 11:02:15 -0600

    practica15 11:02

--- practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
+++ practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -6,14 +6,20 @@ public class RunHilos{
 	JLabel elem1, JLabel elem2){
 		char ch1, ch2;
 		ch1 = txt1.getText().charAt(0);
-		ch1 = txt1.getText().charAt(0);
+		ch2 = txt1.getText().charAt(0);
 		if (ch1 =='\0' || ch1 == ' ') {
 			ch1 = 'E';
 		}
 		if (ch2 =='\0' || ch2 == ' ') {
-			ch1 = 'M';
+			ch2 = 'M';
 		}
-		String salida2 = String.valueOf(ch1);
+		String salida1 = String.valueOf(ch1);
 		String salida2 = String.valueOf(ch2);
+		
+		elem1.setText(salida1);
+		elem2.setText(salida2);
+
+		Letra hilo1 = new Letra(elem1, ch1);
+		Letra hilo2 = new Letra(elem2, ch2);
 	}
 }
